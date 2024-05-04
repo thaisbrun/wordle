b905eb21a18380fea7389124dad62141c3e08b6b
Thaïs Brun <travailthais@outlook.fr>
2024-05-04 22:20:07 +0200

    javascript

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const state = {
     currentRow: 0,
     currentCol: 0,
 };
+function updateGrid(){
+    for(let i=0; i < state.grid.length; i++){
+        for(let j=0; j< state.grid[i].length; j++){
+            const box = document.getElementById(`box${i}${j}`);
+            box.textContent = state.grid[i][j];
+        }
+    }
+}
 function drawBox(container, row, col, letter = ''){
     const box = document.createElement('div');
     box.className = 'box';
@@ -26,9 +34,36 @@ function drawGrid(container){
     container.appendChild(grid);
 }
 
+function registerKeyboardEvents(){
+    document.body.onkeydown = (e) => {
+        const key = e.key;
+        if(key === 'Enter'){
+            if(state.currentCol === 5){
+                const word = getCurrentWord();
+                if(isWordValid(word)){
+                    revealWord(word);
+                    state.currentRow++;
+                    state.currentCol = 0;
+                }else{
+                    alert('Le mot existe pas, Tocard. ')
+                }
+            }
+
+        }if(key === 'Backspace'){
+            removeLetter();
+        }if(isLetter(key)){
+            addLetter(key);
+        }
+        updateGrid();
+    };
+}
+
 function startup(){
     const game = document.getElementById('game');
     drawGrid(game);
+
+    registerKeyboardEvents();
+
 }
 
 startup();
